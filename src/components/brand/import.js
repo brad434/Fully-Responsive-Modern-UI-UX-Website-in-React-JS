7ecc307909f9f182eadb7282e82a4ac15ecf3de2
Bradley Chery <Bradley434@hotmail.com>
2023-03-30 01:48:28 -0400

    Brand section completed

---
+++ src/components/brand/import.js
@@ -0,0 +1,9 @@
+import google from "../../assets/google.png";
+import slack from "../../assets/slack.png";
+import atlassian from "../../assets/atlassian.png";
+import dropbox from "../../assets/dropbox.png";
+import shopify from "../../assets/shopify.png";
+
+export { google, slack, atlassian, dropbox, shopify };
+
+// all images are placed here and associated with a import name for use in the jsx file
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -59,20 +59,19 @@
 
 // export default Navbar;
 
-
-import React from 'react';
-// import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Navbar = () => {
-    // const [toggleMenu, setToggleMenu] = useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
 
     return (
         <div className="gpt3__navbar">
             <div className="gpt3__navbar-links">
                 <div className="gpt3__navbar-links_logo">
-                    <img src={logo} alt="logo" />
+                    <img src={logo} />
                 </div>
                 <div className="gpt3__navbar-links_container">
                     <p><a href="#home">Home</a></p>
@@ -86,7 +85,7 @@ const Navbar = () => {
                 <p>Sign in</p>
                 <button type="button">Sign up</button>
             </div>
-            {/* <div className="gpt3__navbar-menu">
+            <div className="gpt3__navbar-menu">
                 {toggleMenu
                     ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
                     : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
@@ -105,7 +104,7 @@ const Navbar = () => {
                         </div>
                     </div>
                 )}
-            </div> */}
+            </div>
         </div>
     );
 };
